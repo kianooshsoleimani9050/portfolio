e8f13cd7645affc50e8e38749775af8a39b367cb
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2022-01-06 20:55:46 +0330

    feat: :tada: Begining project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "aos/dist/aos.css";
+import "../assets/sass/main.scss";
+import "swiper/css";
+import "swiper/css/bundle";
+import "react-toastify/dist/ReactToastify.css";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
