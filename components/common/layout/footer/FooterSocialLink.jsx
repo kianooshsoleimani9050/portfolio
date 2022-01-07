767c1d44b738dad3776be1ab194532b867e72f2c
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2022-01-07 22:24:10 +0330

    feat: :sparkles: a social component for hilding links in footer

---
+++ components/common/layout/footer/FooterSocialLink.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+const FooterSocialLink = ({
+  children,
+  href = "",
+  className = "",
+  download = false,
+}) => {
+  return (
+    <div className={className}>
+      <Link href={href}>
+        <a
+          target="_blank"
+          referrerPolicy="no-referrer"
+          download={download}
+          className={`footer-social-link d-flex align-items-center justify-content-center`}
+        >
+          <>{children}</>
+        </a>
+      </Link>
+    </div>
+  );
+};
+
+export default FooterSocialLink;
