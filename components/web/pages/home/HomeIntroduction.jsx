329230bf8b7a286bf2353c6f5ceef159b97e06b9
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2022-01-07 22:26:16 +0330

    feat: :sparkles: introduction component for home page

---
+++ components/web/pages/home/HomeIntroduction.jsx
@@ -0,0 +1,20 @@
+const HomeIntroduction = () => {
+  return (
+    <div className="p-2 p-md-0 py-2 py-md-5 m-0 mb-4 my-md-5 home-introduction-container">
+      {"Hey, I'm"}
+      <h1 className="home-introduction-name-text">Kianoosh Soleimani</h1>
+      <b className="home-introduction-name-text-b">
+        React js Front-end developer
+      </b>{" "}
+      with 1+ years of experience in developing projects like CRM, CMS, Blogging
+      sites, Online stores, etc...
+      <br />
+      <b>Self-learner</b>, <b>Teamwork person</b>,{" "}
+      <b>Discipline and Punctuality in work</b>, <b>love new challenges</b>,{" "}
+      <b>calm under pressure</b>, and following the Cutting-edge technologies to
+      improve me.
+    </div>
+  );
+};
+
+export default HomeIntroduction;
