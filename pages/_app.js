aeddf86af99e9dfcf4b0898b95c8373122ce170b
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2022-01-07 22:26:30 +0330

    feat: :sparkles: add weblayout

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,19 @@ import "../assets/sass/main.scss";
 import "swiper/css";
 import "swiper/css/bundle";
 import "react-toastify/dist/ReactToastify.css";
+import WebLayout from "../components/common/layout/WebLayout";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* <Head>
+        <link rel="shortcut icon" href={""} />
+      </Head> */}
+      <WebLayout>
+        <Component {...pageProps} />
+      </WebLayout>
+    </>
+  );
 }
 
 export default MyApp;
