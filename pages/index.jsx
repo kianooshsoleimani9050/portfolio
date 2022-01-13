02f51fce4c32b11afdc6958b66144b8b1cff69ae
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2022-01-13 19:30:41 +0330

    feat: :dizzy: Add animation on scroll
    animate diffrent sections in home page with aos

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,15 @@
+import { useEffect } from "react";
+import AOS from "aos";
+import Experience from "../components/web/pages/home/Experience";
 import HomeIntroduction from "../components/web/pages/home/HomeIntroduction";
-
 export default function Home() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <>
       <HomeIntroduction />
+      <Experience />
     </>
   );
 }
