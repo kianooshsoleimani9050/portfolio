305940b8287b434deb63b2986a14923ef4a41d9e
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2022-01-13 19:28:46 +0330

    feat: data for experience section

---
+++ data/experience-data.js
@@ -0,0 +1,85 @@
+export const EXPERIENCE_DATA = [
+  {
+    id: 1,
+    title: "Front-end developer",
+    date: "2021-Present",
+    name: "Admin panel",
+    where: "Hillz company, Canada ",
+    desc: (
+      <>
+        This project is a <b>React-based admin panel</b> for the management of a
+        car dealership that contains parts like managing vehicles data, handling
+        the finances around negotiating vehicles, <b>customer relationship</b>
+        management <b>(CRM)</b>, controlling their site and blogs, Multilayer
+        structure for gaining level of a simple customer to buyer, Role base
+        login with using a <b>Token authorization</b> that user can change their
+        roles base on requirement.
+      </>
+    ),
+  },
+  {
+    id: 2,
+    title: "Front-end developer",
+    date: "2021-Present",
+    name: "Hillz website",
+    where: "Hillz company, Canada ",
+    desc: (
+      <>
+        Hillz website is a <b>Static Site Generation (SSG)</b> website using the{" "}
+        <b>Next js framework</b> that contains information about their company,
+        forms, packages that they offer to customers(Dealerships), and a blog
+        section for SEO purposes.
+        <br />
+        <b>
+          I gained the experience of operating and deploying SSG websites in
+          this project.
+        </b>
+      </>
+    ),
+  },
+  {
+    id: 3,
+    title: "Front-end developer",
+    date: "2021-Present",
+    name: "Hillz dealership website",
+    where: "Hillz company, Canada ",
+    desc: (
+      <>
+        This project is a <b>Server-Side Rendering (SSR)</b> website using the
+        Next js framework.
+        <br />
+        In this project, we try to use the ability of SSR in Next js for
+        creating dynamic sections based on what the dealership wants to show to
+        people and their customers. Dynamic parts of this project come from our
+        back-end that uses <b>Node js and Express</b> framework for the{" "}
+        <b>back-end</b> server and <b>MySQL</b> for the database. With this
+        config, we can use this project as a Template for other dealerships that
+        like this design for their website and do not require massive changes in
+        the front-end, just creating a table in the database for them and
+        putting their information in that.
+      </>
+    ),
+  },
+  {
+    id: 4,
+    title: "Front-end developer",
+    date: "2021-2022",
+    name: "Bonte takeaway website",
+    where: "Freelancer",
+    desc: (
+      <>
+        Bonte takeaway is a Caffe in shiraz that 5 of my friends established. I
+        tried to help them share their menu with customers in an easy way.
+        <br />
+        This website is generated with <b>React js</b> framework and the
+        challenging part of this project was handling the menu images to get
+        from a <b>CDN (Cloudinary)</b> and display it on their site.
+        <br />
+        <b>
+          This was a good experience for me because I learned how CDNs works and
+          how we can use them in our projects
+        </b>
+      </>
+    ),
+  },
+];
